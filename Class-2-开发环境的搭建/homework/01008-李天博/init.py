6b83605562b359635df2ac6c8c96fee10bc28d9c
Joe_Lee <690170170@qq.com>
2017-12-13 15:42:42 +0800

    李天博的init

---
+++ Class-2-开发环境的搭建/homework/01008-李天博/init.py
@@ -0,0 +1,13 @@
+import nuke
+
+nuke.pluginAddPath('//192.168.0.2/PythonDev/BC_ToolSet')
+nuke.pluginAddPath('//192.168.0.2/PythonDev/NukeToolSet')
+nuke.pluginAddPath('//192.168.0.2/PythonDev/Pr_Suite')
+nuke.pluginAddPath('//192.168.0.2/PythonDev/PLUS1.0_Beta')
+nuke.pluginAddPath('//192.168.0.2/PythonDev/Distortion')
+
+
+
+
+
+
