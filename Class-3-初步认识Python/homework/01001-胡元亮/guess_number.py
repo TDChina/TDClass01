b33d6055b1f1c888ca6d1e551d239012709d0246
Sam <562692048@qq.com>
2017-12-14 23:03:16 +0800

    homework commit: control flow

---
+++ Class-3-初步认识Python/homework/01001-胡元亮/guess_number.py
@@ -0,0 +1,14 @@
+﻿# -*- coding: utf-8 -*-
+
+import random
+from __builtin__ import raw_input
+
+if __name__ == "__main__":
+    goal = str(random.randint(1, 100))
+    num_input = raw_input("Please type a number: ")
+    
+    while goal != num_input:
+        s = "smaller" if goal > num_input else "bigger"
+        num_input = raw_input("Your number is %s than the target. Please try again: " % s)
+        
+    print "Congratulations!"
---
+++ Class-3-初步认识Python/homework/01001-胡元亮/guess_number_reversed.py
@@ -0,0 +1,20 @@
+﻿# -*- coding: utf-8 -*-
+
+import random
+from __builtin__ import raw_input
+
+if __name__ == "__main__":
+    num_min, num_max = 1, 100
+    guess = random.randint(num_min, num_max)
+    result = raw_input("I guess the number is %s, is that correct? [Y/B/S] " % guess)
+    
+    while not result in ["y", "Y"]:
+        if result in ["b", "B"] and max > guess:
+            num_max = guess - 1
+        elif result in ["s", "S"] and min < guess:
+            num_min = guess + 1
+        
+        guess = random.randint(num_min, num_max)
+        result = raw_input("I guess the number is %s, is that correct? [Y/B/S] " % guess)
+        
+    print "Yahoo!"
