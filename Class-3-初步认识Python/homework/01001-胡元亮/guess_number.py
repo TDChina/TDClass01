56365916b44daf288b70b36933c8d9e7a3de0943
Sam <562692048@qq.com>
2017-12-15 12:46:08 +0800

    improved

--- Class-3-初步认识Python/homework/01001-胡元亮/guess_number.py
+++ Class-3-初步认识Python/homework/01001-胡元亮/guess_number.py
@@ -1,14 +1,15 @@
 ﻿# -*- coding: utf-8 -*-
 
 import random
-from __builtin__ import raw_input
 
 if __name__ == "__main__":
     goal = str(random.randint(1, 100))
     num_input = raw_input("Please type a number: ")
-    
-    while goal != num_input:
+    chances = 5
+
+    while goal != num_input and chances:
+        chances -= 1
         s = "smaller" if goal > num_input else "bigger"
         num_input = raw_input("Your number is %s than the target. Please try again: " % s)
-        
-    print "Congratulations!"
+
+    print "Congratulations!" if chances else "Sorry! The answer is %s." % goal
