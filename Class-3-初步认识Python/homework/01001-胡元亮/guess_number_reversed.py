6f8e9113c2551e15d0e7fc727a729435df001eb1
Sam <562692048@qq.com>
2017-12-15 12:35:37 +0800

    improved

--- Class-3-初步认识Python/homework/01001-胡元亮/guess_number_reversed.py
+++ Class-3-初步认识Python/homework/01001-胡元亮/guess_number_reversed.py
@@ -1,20 +1,19 @@
 ﻿# -*- coding: utf-8 -*-
 
 import random
-from __builtin__ import raw_input
 
 if __name__ == "__main__":
     num_min, num_max = 1, 100
-    guess = random.randint(num_min, num_max)
-    result = raw_input("I guess the number is %s, is that correct? [Y/B/S] " % guess)
-    
-    while not result in ["y", "Y"]:
-        if result in ["b", "B"] and max > guess:
-            num_max = guess - 1
-        elif result in ["s", "S"] and min < guess:
-            num_min = guess + 1
-        
+    result, guess = "", 0
+
+    while result not in ["y", "Y"]:
+
+        if result in ["b", "B"]:
+            num_max = max(guess - 1, num_min)
+        elif result in ["s", "S"]:
+            num_min = min(guess + 1, num_max)
+
         guess = random.randint(num_min, num_max)
         result = raw_input("I guess the number is %s, is that correct? [Y/B/S] " % guess)
-        
+
     print "Yahoo!"
