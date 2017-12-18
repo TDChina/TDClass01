c359dd52c8857f52c4bb60bf00511548b473efb9
cgpengda <cgpengda@gmail.com>
2017-12-18 20:43:52 +0800

    Update human_guess_numbers.py
    添加一个判断，判断输入的字符串只包含数字！

--- Class-3-初步认识Python/homework/01003-彭达/human_guess_numbers.py
+++ Class-3-初步认识Python/homework/01003-彭达/human_guess_numbers.py
@@ -11,15 +11,18 @@ def main():
     count = 1
     while count:
         input_num_str = raw_input(u"请输入一个0到99之间的整数：")
-        input_num = int(input_num_str)
-        if input_num > rand_num:
-            print u"大了，再猜猜！"
-        elif input_num < rand_num:
-            print u"小了，再猜猜！"
+        if input_num_str.isdigit():
+            input_num = int(input_num_str)
+            if input_num > rand_num:
+                print u"大了，再猜猜！"
+            elif input_num < rand_num:
+                print u"小了，再猜猜！"
+            else:
+                print u"答对了！"
+                break
+            count += 1
         else:
-            print u"答对了！"
-            break
-        count += 1
+            print u"请输入一个整数!"
 
 
 if __name__ == '__main__':
