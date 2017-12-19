6bcb103b81c7b4b7ab9fd259b1cd6135cdd00381
Xuefeng Zhang <645243825@qq.com>
2017-12-19 20:16:02 +0800

    Class3:homework.V02张雪峰提交

--- Class-3-初步认识Python/homework/01009-张雪峰/guess_number_reversed.py
+++ Class-3-初步认识Python/homework/01009-张雪峰/guess_number_reversed.py
@@ -1,34 +1,26 @@
 # *_*coding:utf-8 *_*
-b=1
-while b==1:
+frequency = 1
+while frequency == 1:
     in_num = raw_input('请输入一个100以内的整数：')
-    if in_num.isdigit() == True  and int(in_num) < 100 and int(in_num) > 0:
-        in_num=int(in_num)
-        c=1
+    if in_num.isdigit() == True and (0 < int(in_num) < 100):
+        in_num = int(in_num)
+        frequency_1 = 1
         num_max = 99
-        num_min = 99
-        num_var = 0
-        while c == 1:
-            if num_min == in_num:
-                print(num_min)
-                b = 0
-                c = 0
-            elif num_var == in_num:
-                print(num_var)
-                b = 0
-                c = 0
-            elif num_min > in_num or num_var > in_num:
-                if num_var == 0:
-                    num_max = num_min
-                    num_min = num_max // 2
-                else:
-                    num_max = num_var
-                    num_var = (num_max - num_min) // 2 + num_min
+        num_min = 0
+        num_mid = 50
+        Times = 0
+        while frequency_1 == 1:
+            if num_mid == in_num:
+                print("%s, 通过 %s 次计算得出" %(num_mid, Times))
+                frequency_1 = 0
+                frequency = 0
+            elif num_mid > in_num:
+                Times += 1
+                num_max = num_mid
+                num_mid = (num_max + num_min) / 2
             else:
-                if num_var == 0:
-                    num_var = (num_max - num_min) // 2 + num_min
-                else:
-                    num_min = num_var
-                    num_var = (num_max - num_min) // 2 + num_min
+                Times += 1
+                num_min = num_mid
+                num_mid = (num_max + num_min) / 2
     else:
         print "非法输入!!!!",
