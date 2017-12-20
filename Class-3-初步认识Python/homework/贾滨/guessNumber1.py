b866c7f71d980e672930c486adb94b214bfe2310
real <jiabintd@gmail.com>
2017-12-20 10:48:38 +0800

    贾滨修改了 猜数字

--- Class-3-初步认识Python/homework/贾滨/guessNumber1.py
+++ Class-3-初步认识Python/homework/贾滨/guessNumber1.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 import random
-  
+
+
 number = random.randint(0, 100)
-print "测试随值=>", number, "<=你看不见我\n"
+# print "测试随值=>", number
 count, more = 0, 5
 while count <= more:
-    # 循环计数器
+    # 次数小于或等于就循环
     while True:
-        # 循环非数字
+        # 循环用户 输入非数字
         user_number = raw_input("请输入0-100间的整数：")
         if user_number.isdigit():
             user_number = int(user_number)
@@ -19,19 +20,19 @@ while count <= more:
         print "对不起，大了。你还有[", more - count, "]次机会。"
     elif user_number < number:
         print "对不起, 小了。你还有[", more - count, "]次机会。"
-    elif user_number == number:
+    else:
         print "恭喜你答对了。"
         break
 
-    if count == more:
-        while count == more:
-            # 循环 继续 与 退出 非指定字符
-            user_continue_out = raw_input("'y'继续，'n'退出：")
-            if user_continue_out == "y" or user_continue_out == "n":
-                if user_continue_out == "y":
-                    count = 0
-                elif user_continue_out == "n":
-                    break
-            else:
-                print "错误:只能输入一个小写字母'y'或'n'"
+    while count == more:
+        # 循环 继续 与 退出 非指定字符
+        user_continue_out = raw_input("'y'继续，'n'退出：")
+        if user_continue_out == "y" or user_continue_out == "n":
+            if user_continue_out == "y":
+                count = 0
+            elif user_continue_out == "n":
+                break
+        else:
+            print "错误:只能输入一个小写字母'y'或'n'"
+
     count += 1
